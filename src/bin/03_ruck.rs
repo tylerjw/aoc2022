a78f91434cc8136713ec6eb40dfac27ab8d30b56
Tyler Weaver <maybe@tylerjw.dev>
2022-12-03 20:52:18 -0700

    Day 3: Rucksack Reorganization - part 2
    Signed-off-by: Tyler Weaver <maybe@tylerjw.dev>

--- src/bin/03_ruck.rs
+++ src/bin/03_ruck.rs
@@ -1,6 +1,20 @@
+#![feature(iter_array_chunks)]
 use std::collections::hash_set::HashSet;
 use std::env;
 use std::fs;
+
+fn score(items: &str) -> i32 {
+    items
+        .chars()
+        .map(|item| {
+            match item as i32 {
+                n @ 97..=122 => n - 96, // lower case
+                n @ 65..=90 => n - 38,  // upper case
+                _ => panic!("nooooo"),
+            }
+        })
+        .sum::<i32>()
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -19,17 +33,29 @@ fn main() {
         })
         .collect::<String>();
 
-    let priority_sum = duplicate_items
-        .chars()
-        .map(|item| {
-            match item as i32 {
-                n @ 97..=122 => n - 96, // lower case
-                n @ 65..=90 => n - 38,  // upper case
-                _ => panic!("nooooo"),
-            }
-        })
-        .sum::<i32>();
+    let priority_sum = score(&duplicate_items);
 
     dbg!(duplicate_items);
     dbg!(priority_sum);
+
+    let group_badges = rucksack
+        .iter()
+        .array_chunks::<3>()
+        .map(|group| {
+            group[0]
+                .chars()
+                .collect::<HashSet<_>>()
+                .intersection(&group[1].chars().collect::<HashSet<_>>())
+                .collect::<String>()
+                .chars()
+                .collect::<HashSet<_>>()
+                .intersection(&group[2].chars().collect::<HashSet<_>>())
+                .collect::<String>()
+        })
+        .collect::<String>();
+
+    let group_score = score(&group_badges);
+
+    dbg!(group_badges);
+    dbg!(group_score);
 }
